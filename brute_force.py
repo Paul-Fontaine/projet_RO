d7ab7e000235ed585c09f5d1436e200d83f37ecf
Paul Fontaine <fontainepaul36@gmail.com>
2024-06-04 10:44:44 +0200

    store the list of items in a dict in a file. U can import it from everywhere an don't have to use pandas with an excel file anymore

--- brute_force.py
+++ brute_force.py
@@ -1,14 +1,5 @@
-import pandas as pd
 import time
-
-df = pd.read_excel("items.xlsx")
-items = {
-    "name": df['Objet'].tolist(),
-    "weight": df['Masse'].tolist(),
-    "utility": df['Utilité'].tolist()
-}
-print()
-items = [{'name': items['name'][i], 'weight': items['weight'][i], 'utility': items['utility'][i], } for i in range(len(items['name']))]
+from items_dict import items_dict
 
 
 def make_combinations(items, W: float):
@@ -27,11 +18,12 @@ def find_max_utility(combinations):
     return max(combinations, key=lambda c: c['weight'])
 
 
-def find_best_bag(items=items, W: float = 0.6):
+def find_best_bag(items, W: float = 0.6):
     return find_max_utility(make_combinations(items, W+0.0001))
 
+
 s_time = time.time()
-best_bag = find_best_bag(items)
+best_bag = find_best_bag(items_dict)
 e_time = time.time()
 
 print(f"{e_time-s_time} s")
